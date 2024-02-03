061be0c9e8a06f6b9850c447032bfdc2184e5c31
nikhilmaurya03 <nikhil56kumar8767@gmail.com>
2024-02-03 22:34:29 +0530

    second commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+      int num1=10, num2=20, sum;
+        System.out.println("Hello and welcome Nikhil!");
+        System.out.print("the sum is:- ");
+        sum = num1 + num2;
+        System.out.print(sum);
+    }
+}
